5f29b54976e7871f5eb7701830db9e55c98a80a3
Antonio Ramón Molina Milla <armolinamilla@gmail.com>
2022-02-07 17:32:03 +0100

    create proto and pb2 for managers

--- srv_reader/main.py
+++ srv_reader/main.py
@@ -1,8 +1,13 @@
 import sys
-
+import csv
 
 def main():
-    print("hello!")
+    f = open("/tmp/data/Managers.csv")
+    dataReader = csv.reader(f, delimiter=',')
+    for row in dataReader:
+        print(row)
+
+    print("Service finish!")
 
 if __name__ == "__main__":
     main()
