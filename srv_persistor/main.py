634f7217c0c0046a71d558cd4a6974e062b33f98
Antonio Ramón Molina Milla <armolinamilla@gmail.com>
2022-02-12 11:06:25 +0100

    create client and server

---
+++ srv_persistor/main.py
@@ -0,0 +1,26 @@
+import sys
+import csv
+import grpc
+import managers_pb2
+import managers_pb2_grpc
+
+from concurrent import futures
+
+class Manager(managers_pb2_grpc.ManagerServicer):
+    def PingManagers(self, request, context):
+        return managers_pb2.managers_pb2.ManagerPingResponse("True")
+    
+
+def main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    managers_pb2_grpc.add_ManagerServicer_to_server(Manager(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("server started, port: 50051")
+    server.wait_for_termination()
+
+    print("Service finish!")
+
+if __name__ == "__main__":
+    main()
--- srv_reader/main.py
+++ srv_reader/main.py
@@ -1,11 +1,17 @@
 import sys
 import csv
+import grpc
 
+import managers_pb2_grpc
+import managers_pb2
 def main():
     f = open("/tmp/data/Managers.csv")
     dataReader = csv.reader(f, delimiter=',')
-    for row in dataReader:
-        print(row)
+
+    with grpc.insecure_channel('srv_persistor:50051') as channel:
+        stub = managers_pb2_grpc.ManagerStub(channel)
+        response = stub.PingManagers(managers_pb2.EmptyMesssage())
+    print("Mensaje recibido: " + response.message)
 
     print("Service finish!")
 
