d3660c5b9cb96356ccbd9dedc0f0e991dcb77688
Antonio Ramón Molina Milla <armolinamilla@gmail.com>
2022-02-19 19:08:19 +0100

    read correct file with several rows

--- srv_reader/main.py
+++ srv_reader/main.py
@@ -2,18 +2,27 @@ import sys
 import csv
 import grpc
 
+from repository.file_repository import FileRepository
+from dto.sales_record import SaleRecord
+
 import managers_pb2_grpc
 import managers_pb2
 
 def main():
-    f = open("/tmp/data/Managers.csv")
-    dataReader = csv.reader(f, delimiter=',')
 
-    with grpc.insecure_channel('srv_persistor:50051') as channel:
-        print("srv_status: " + grpc.RpcError)
-        stub = managers_pb2_grpc.ManagerStub(channel)
-        response = stub.PingManagers(managers_pb2.EmptyMesssage())
-    print("Mensaje recibido: " + response.result)
+    file_repository = FileRepository("./srv_reader/small_sales_records.csv")
+    data_readed = file_repository.read_data()
+
+    sales_records = []
+
+    for row in data_readed:
+            sale_record = SaleRecord(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13])
+            sales_records.append(sale_record)
+
+    ##with grpc.insecure_channel('srv_persistor:50051') as channel:
+    ##    stub = managers_pb2_grpc.ManagerStub(channel)
+    ##    response = stub.PingManagers(managers_pb2.EmptyMesssage())
+    ##print("Mensaje recibido: " + response.result)
 
     print("Service finish!")
 
--- srv_reader/repository/file_repository.py
+++ srv_reader/repository/file_repository.py
@@ -1,33 +1,33 @@
-import json
-from src.shop.dto.product import Product
+import csv
 
 class FileRepository():
-    __file_path = 'src/resources/product_list.json'
+    __file_path = ''
     __file_open = None
+    
+    def __init__(self, file_path):
+        self.__file_path = file_path
 
     def __open_file(self) -> None:
         if(self.__file_open==None):
             self.__file_open = open(self.__file_path)
+    
+    def __close_file(self) -> None:
+        if(self.__file_open):
+            self.__file_open.close()
 
     def read_data(self):
         try:
+            data_read = []
+            
             self.__open_file()
-            data_read = json.load(self.__file_open)
+            csv_reader = csv.reader(self.__file_open)
+            
+            next(csv_reader)
+            for row in csv_reader:
+                    data_read.append(row)
+                    
             return data_read
         except Exception as exception:
             raise Exception('Error reading data: ' + str(exception))
         finally:
             self.__close_file()
-
-    def find_by_code(self, code: str) -> Product:
-        data = self.read_data()
-
-        for item in data:
-            if(item['code']==code):
-                return item
-
-        return None
-
-    def __close_file(self) -> None:
-        if(self.__file_open):
-            self.__file_open.close()
