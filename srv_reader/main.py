dc76b2cf04168b6a08cbc9bfeac144855880f51c
armolinamilla <armolinamilla@gmail.com>
2022-02-25 23:19:43 +0100

    adding makefiles

--- srv_reader/main.py
+++ srv_reader/main.py
@@ -16,14 +16,15 @@ def main():
     sales_records = []
 
     for row in data_readed:
-            sale_record = SaleRecord(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13])
-            sales_records.append(sale_record)
+        sale_record = SaleRecord(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13])
+        sales_records.append(sale_record)
 
-    ##with grpc.insecure_channel('srv_persistor:50051') as channel:
-    ##    stub = managers_pb2_grpc.ManagerStub(channel)
-    ##    response = stub.PingManagers(managers_pb2.EmptyMesssage())
-    ##print("Mensaje recibido: " + response.result)
-
+        with grpc.insecure_channel('srv_persistor:50051') as channel:
+            stub = managers_pb2_grpc.ManagerStub(channel)
+            response = stub.PingManagers(managers_pb2.EmptyMesssage())
+            print("Mensaje recibido: " + response.result)
+    
+    channel.close()
     print("Service finish!")
 
 if __name__ == "__main__":
